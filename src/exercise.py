aa2580877189129baa8095f13697c81f6aed077b
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 18:55:56 +0000

    Solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
+
 def main():
     #write your code below this line
+    acc1 = Account("Matthew's account",1000)
+    acc2 = Account("My account",0)
+    acc1.withdraw(100)
+    acc2.deposit(100)
+    print(acc1)
+    print(acc2)
+    
 
 if __name__ == '__main__':
+    from account import Account
     main()
+else:
+    from src.account import Account
